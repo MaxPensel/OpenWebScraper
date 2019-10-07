01862011dd27634b4fb707c0f569576dcc82fb18
Maximilian Pensel <maximilian.pensel@gmx.de>
2019-10-07 11:03:12 +0200

    Apply GPLv3
    Add COPYING
    Add license notice in all source files
    Remove Apache 2.0 license

--- modules/template/view.py
+++ modules/template/view.py
@@ -1,10 +1,27 @@
 """
-Created on 14.06.2019
-
 Use this as a cheat sheet for setting up the layout of the main widget of a module.
 See __init__.py for information on how to setup modules.
 
+Created on 14.06.2019
+
 @author: Maximilian Pensel
+
+Copyright 2019 Maximilian Pensel <maximilian.pensel@gmx.de>
+
+This file is part of OpenWebScraper.
+
+OpenWebScraper is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+OpenWebScraper is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with OpenWebScraper.  If not, see <https://www.gnu.org/licenses/>.
 """
 from core.QtExtensions import VerticalContainer, HorizontalContainer, FileOpenPushButton
 
